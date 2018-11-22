08ef4dc8b8448568f4e0758c73a0ed9dd7d28724
DeadmanIQ445 <35527371+DeadmanIQ445@users.noreply.github.com>
2018-11-22 13:49:52 +0300

    Another change in collation

--- Dump20181122.sql
+++ Dump20181122.sql
@@ -196,7 +196,7 @@ CREATE TABLE `provider` (
   PRIMARY KEY (`pid`),
   UNIQUE KEY `pid_UNIQUE` (`pid`),
   UNIQUE KEY `zipcode_UNIQUE` (`zipcode`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -421,4 +421,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-22 13:04:46
+-- Dump completed on 2018-11-22 13:43:14
